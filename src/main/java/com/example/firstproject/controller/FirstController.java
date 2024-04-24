13b57b06de1f62b486c1155b2d1e2080e13ee6f0
lkh2919 <lkh2919@naver.com>
2024-04-24 23:49:32 +0900

    second commit

--- src/main/java/com/example/firstproject/controller/FirstController.java
+++ src/main/java/com/example/firstproject/controller/FirstController.java
@@ -1,2 +1,18 @@
-package com.example.firstproject.controller;public class FirstController {
+package com.example.firstproject.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FirstController {
+
+    @GetMapping("/hi")
+    public String niceToMeetYou(Model model) {
+
+        model.addAttribute("username","Kevin");
+        return "greetings";
+    }
+
 }
